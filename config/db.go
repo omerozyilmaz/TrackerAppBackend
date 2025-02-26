3ecdd3137e11efd0c2562feade4f5367fd4878f2
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2025-02-26 12:32:54 +0300

    Refactor database connection to use DATABASE_URL

--- config/db.go
+++ config/db.go
@@ -1,9 +1,7 @@
 package config
 
 import (
-	"fmt"
 	"job-tracker-api/models"
-	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -13,27 +11,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() (*gorm.DB, error) {
-	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	// Create or update the jobs table
-	err = DB.AutoMigrate(&models.User{}, &models.Job{})
-	if err != nil {
-		log.Fatal("Failed to migrate database schema:", err)
-	}
+	// AutoMigrate işlemi burada yapılabilir
+	// err = db.AutoMigrate(&models.User{}, &models.Job{})
+	// if err != nil {
+	//     return nil, err
+	// }
 
-	return DB, nil
+	return db, nil
 }
 
 // Yeni kullanıcı oluşturulduğunda çağrılacak fonksiyon
