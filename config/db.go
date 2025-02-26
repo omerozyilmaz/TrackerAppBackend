42af069cba0b0f43e06ed1d6f90bfa08de4fdd80
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2025-02-26 15:35:26 +0300

    Update

--- config/db.go
+++ config/db.go
@@ -14,17 +14,27 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
+	// Railway'de DATABASE_URL ortam değişkeni otomatik olarak sağlanır
+	dbURL := os.Getenv("DATABASE_URL")
+	
+	// Eğer DATABASE_URL varsa, doğrudan onu kullan
+	if dbURL != "" {
+		DB, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	} else {
+		// Yoksa, ayrı ayrı değişkenleri kullan (yerel geliştirme için)
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	}
+	
 	if err != nil {
-		log.Fatal("Failed to connect to database")
+		log.Fatal("Failed to connect to database:", err)
 	}
 
 	// Create or update the jobs table
